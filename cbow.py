d60fa12cc45f443ef6d7525d964e6829e6192caa
Wuelle <58120269+Wuelle@users.noreply.github.com>
2020-06-21 13:48:36 +0200

    Cleaned up code and removed unnecessary dependencies
    1. Removed numpy dependency as the conversion between a pytorch tensor and a numpy array is unnecessary
    2. Removed the custom argmax function as pytorch already has the builtin method torch.argmax(tensor)
    3. Changed string formatting from .format() to f-strings
    4. Cleaned up the model optimization process(once per epoch)
    5. Removed get_max_prob_result as its a one liner thats readable without a seperate function
    6. Cleaned up the word_to_ix and ix_to_word dictionary creation

--- cbow.py
+++ cbow.py
@@ -1,27 +1,12 @@
 import torch
 import torch.nn as nn
-import numpy as np
 
 def make_context_vector(context, word_to_ix):
     idxs = [word_to_ix[w] for w in context]
     return torch.tensor(idxs, dtype=torch.long)
 
-def get_index_of_max(input):
-    index = 0
-    for i in range(1, len(input)):
-        if input[i] > input[index]:
-            index = i 
-    return index
-
-def get_max_prob_result(input, ix_to_word):
-    return ix_to_word[get_index_of_max(input)]
-
-
 CONTEXT_SIZE = 2  # 2 words to the left, 2 to the right
 EMDEDDING_DIM = 100
-
-word_to_ix = {}
-ix_to_word = {}
 
 raw_text = """We are about to study the idea of a computational process.
 Computational processes are abstract beings that inhabit computers.
@@ -35,9 +20,8 @@ we conjure the spirits of the computer with our spells.""".split()
 vocab = set(raw_text)
 vocab_size = len(vocab)
 
-for i, word in enumerate(vocab):
-    word_to_ix[word] = i
-    ix_to_word[i] = word
+word_to_ix = {word:ix for ix, word in enumerate(vocab)}
+ix_to_word = {ix:word for ix, word in enumerate(vocab)}
 
 data = []
 for i in range(2, len(raw_text) - 2):
@@ -47,22 +31,17 @@ for i in range(2, len(raw_text) - 2):
     data.append((context, target))
 
 
-
 class CBOW(torch.nn.Module):
-
     def __init__(self, vocab_size, embedding_dim):
         super(CBOW, self).__init__()
 
         #out: 1 x emdedding_dim
         self.embeddings = nn.Embedding(vocab_size, embedding_dim)
-
         self.linear1 = nn.Linear(embedding_dim, 128)
-
         self.activation_function1 = nn.ReLU()
         
         #out: 1 x vocab_size
         self.linear2 = nn.Linear(128, vocab_size)
-
         self.activation_function2 = nn.LogSoftmax(dim = -1)
         
 
@@ -75,7 +54,7 @@ class CBOW(torch.nn.Module):
         return out
 
     def get_word_emdedding(self, word):
-        word = torch.LongTensor([word_to_ix[word]])
+        word = torch.tensor([word_to_ix[word]])
         return self.embeddings(word).view(1,-1)
 
 
@@ -84,24 +63,28 @@ model = CBOW(vocab_size, EMDEDDING_DIM)
 loss_function = nn.NLLLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
 
-
-
+#TRAINING
 for epoch in range(50):
     total_loss = 0
+
     for context, target in data:
         context_vector = make_context_vector(context, word_to_ix)  
-        model.zero_grad()
+
         log_probs = model(context_vector)
-        loss = loss_function(log_probs, torch.tensor([word_to_ix[target]], dtype=torch.long))
-        loss.backward()
-        optimizer.step()
 
-        total_loss += loss.data
+        total_loss += loss_function(log_probs, torch.tensor([word_to_ix[target]]))
 
-# ====================== TEST
+    #optimize at the end of each epoch
+    optimizer.zero_grad()
+    total_loss.backward()
+    optimizer.step()
+
+#TESTING
 context = ['People','create','to', 'direct']
 context_vector = make_context_vector(context, word_to_ix)
-a = model(context_vector).data.numpy()
-print('Raw text: {}\n'.format(' '.join(raw_text)))
-print('Context: {}\n'.format(context))
-print('Prediction: {}'.format(get_max_prob_result(a[0], ix_to_word)))
+a = model(context_vector)
+
+#Print result
+print(f'Raw text: {" ".join(raw_text)}\n')
+print(f'Context: {context}\n')
+print(f'Prediction: {ix_to_word[torch.argmax(a[0]).item()]}')
